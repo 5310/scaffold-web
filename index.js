19e4448792aff163b9bb5c7070ce4df6c2ef3ff9
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2017-09-23 16:05:09 +0530

    🆕

---
+++ index.js
@@ -0,0 +1,10 @@
+if (location.protocol === 'http:') location.protocol = 'https:';
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(reg => console.log('Service Worker registered', reg)).catch(err => console.error('Service Worker **not** registered', err));
+} else {
+  console.warn('Service Worker not supported in this browser');
+}
+
+console.log('Hullo from JavaScript, Web!');
+
+//# sourceMappingURL=index.js.map
