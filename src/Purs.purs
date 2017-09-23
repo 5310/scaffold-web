32222667d14fdcbcbf439b368a34e187ce80e771
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2017-09-23 15:10:59 +0530

    PureScript stuff removed from base project.

--- src/Purs.purs
+++
@@ -1,9 +0,0 @@
-module Purs where
-
-import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
-
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hullo from PureScript, Web!"
--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 import './sw-register.js'
 
-import Purs from './_psc-output/Purs/index.js'
-// import Purs from './Purs.purs' // `rollup-plugin-purs` not yet ready. Lots of errors.
-
 console.log('Hullo from JavaScript, Web!')
-
-Purs.main()
