82cfc84c5d5a0380b95f2b388e3baed2c00f2486
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2017-09-23 11:56:45 +0530

    Dotfiles all configs scripts. Even `gulpfile.js` was renamed to `.gulpfile.js` and alias added to NPM scripts. This is neater.

--- .gulpfile.js
+++ .gulpfile.js
@@ -15,10 +15,10 @@ const watch = require('gulp-watch')
 const pug = require('gulp-pug')
 
 const postcss = require('gulp-postcss')
-const { plugins: postcssPlugins, options: postcssOpts } = require('./postcss.config.js')
+const { plugins: postcssPlugins, options: postcssOpts } = require('./.postcss.config.js')
 
 const rollup = require('gulp-better-rollup')
-const { inputOptions: rollupIOpts, outputOptions: rollupOOpts } = require('./rollup.config.js')
+const { inputOptions: rollupIOpts, outputOptions: rollupOOpts } = require('./.rollup.config.js')
 
 // const babel = require('gulp-babel')
 
