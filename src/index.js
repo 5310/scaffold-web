36bb4cac78f07f2bbdccfa635f2f0dd294d9da2c
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2017-09-23 15:17:15 +0530

    PureScript scaffold added back in.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './sw-register.js'
 
+import Purs from './_psc-output/Purs/index.js'
+// import Purs from './Purs.purs' // `rollup-plugin-purs` not yet ready. Lots of errors.
+
 console.log('Hullo from JavaScript, Web!')
+
+Purs.main()
