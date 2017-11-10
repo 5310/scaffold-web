0b85cff569cb53e82d5851687628de10e3a024c0
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2017-11-10 11:27:42 +0530

    Added postcss-gridlover for as some well-deservered vertical rhythm.

--- .postcss.config.js
+++ .postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-nested-props'),
     require('postcss-nested'),
+    require('postcss-gridlover'),
     require('postcss-mixins'),
     require('postcss-font-magician'),
     require('postcss-cssnext'),
